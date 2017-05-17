d5ccd4fd08ebcaea4af4f6536f890f2b6b2d5367
disne_y <bold.disney@gmail.com>
2017-05-17 17:03:57 +0300

    Minor changes

--- les_02/alphabet.rb
+++ les_02/alphabet.rb
@@ -2,11 +2,8 @@ vowels = %w(a e i o u y)
 letters = ('a'..'z')
 hash = {}
 
-letters.each_with_index do |letter, index|
-  if vowels.include?(letter)
-    hash[letter] = index + 1
-  end
+letters.each.with_index(1) do |letter, index|
+    hash[letter] = index if vowels.include?(letter)
 end
 
 puts hash
-
