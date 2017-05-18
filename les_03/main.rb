19df652423e761362e6d23111ec867f272af6623
disne_y <bold.disney@gmail.com>
2017-05-18 21:12:22 +0300

    Add main.rb

---
+++ les_03/main.rb
@@ -0,0 +1,21 @@
+require_relative 'train.rb'
+require_relative 'route.rb'
+require_relative 'station.rb'
+require_relative 'controller.rb'
+
+def self.user_choice
+  user_choice = gets.to_i
+end
+
+controller = Controller.new
+
+puts 'Добро пожаловать в он-лайн депо'
+
+
+
+loop do
+  controller.main_actions
+  controller.execute_action(user_choice)
+
+
+end
