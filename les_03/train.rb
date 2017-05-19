252f3a7b38c4ca38fed3bea1e79e13d895cf10c7
disne_y <bold.disney@gmail.com>
2017-05-19 20:22:47 +0300

    Display next/prev stations in the route. Train can move beetween stations

--- les_03/train.rb
+++ les_03/train.rb
@@ -10,11 +10,21 @@ class Train
     @carriages_qty = carriages_qty
     @speed = 0
   end
+ 
+  #Блок по управлению движением
+  def increase_speed(value)
+    self.speed = value
+  end
+
+  def current_speed
+    self.speed
+  end
 
   def stop
     self.speed = 0
   end
 
+  #Добавление/отцепка вагонов
   def add_carriage
     if speed == 0 
       self.carriages_qty += 1 
@@ -32,6 +42,7 @@ class Train
     end
   end
 
+  #Присваивание маршрута
   def set_route(route)
     @route = route
     puts "Назначен маршрут: #{route.stations.first} - #{route.stations.last}"
@@ -40,27 +51,60 @@ class Train
     puts "Текущая станция: #{current_station}"
   end
 
-  def increase_speed(value)
-    self.speed = value
+  #Перемещение по маршруту
+  def move(direction)
+    if self.route 
+      if direction == :forward && self.next_station
+        self.increase_speed(20)
+        puts "Вжжжжж..."
+
+        self.current_station = self.next_station
+        self.stop
+
+        puts "Поезд прибыл на станцию: #{current_station}"
+      elsif direction == :back && self.prev_station
+        self.increase_speed(20)
+        puts "Вжжжжж..."
+
+        self.current_station = self.prev_station
+        self.stop
+
+        puts "Поезд прибыл на станцию: #{current_station}"
+      else
+        puts "...движение невозможно"
+      end
+    else
+      puts "Прежде чем начать движение необходимо назначить маршрут."
+    end
   end
 
-  def current_speed
-    self.speed
+
+  #Вспомогательные методы пока не прятал их в private
+  def current_station=(station)
+    @current_station = station
   end
 
-  def current_carriages_qty
-    self.carriages_qty
+  def next_station
+    position = self.route.stations.index(self.current_station)
+
+    if self.current_station == self.route.stations.last
+      puts 'Конечная'
+    else
+      self.route.stations[position + 1] 
+    end
   end
 
-  # def next_station
-  #   self.route.next(self.current_station)
-  # end
+  def prev_station
+    position = self.route.stations.index(self.current_station)
+  
+    if self.current_station == self.route.stations.first
+      puts 'Конечная'
+    else
+      self.route.stations[position - 1] 
+    end
+  end
 
   def about
     puts "Тип: #{TYPE[self.type]}, вагонов: #{self.carriages_qty}, скорость: #{self.speed}"
-  end
-
-  def current_station=(station)
-    @current_station = station
   end
 end
