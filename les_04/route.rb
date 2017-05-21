e620c92603114a3080727fb6439977ef07e610a3
disne_y <bold.disney@gmail.com>
2017-05-21 20:01:38 +0300

    Minor fix

--- les_04/route.rb
+++ les_04/route.rb
@@ -1,8 +1,10 @@
 class Route
-  attr_reader :stations
+  attr_reader :stations, :start_station, :end_station
 
   def initialize(start_station, end_station)
     @stations = [start_station, end_station]
+    @start_station = start_station
+    @end_station = end_station
   end
 
   def stations_list
