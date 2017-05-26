7aaf33cb04d9acf94399902a70aef4163f1e7536
disne_y <bold.disney@gmail.com>
2017-05-26 10:25:59 +0300

    Initial commit for lesson 6

---
+++ les_06/models/station.rb
@@ -0,0 +1,48 @@
+class Station
+  attr_reader :title, :trains
+
+  @@stations= []
+
+  def initialize(title)
+    @title = title
+    @trains = []
+    @@stations << self
+  end
+
+  def self.all
+    @@stations
+  end
+
+  def trains_at_the_station
+    if self.trains.empty?
+      puts '  ..поездов нет.'
+    else
+      puts '  ..cписок поездов на станции:'
+      self.trains.each do |train|
+        puts "\tпоезд №: #{train.number}"
+        puts "\tтип: #{Train::TYPE[train.type]}"
+        puts "\tкол-во вагонов: #{train.carriages.count}"
+        puts "\tмаршрут следования: #{train.route.stations.first.title} - #{train.route.stations.last.title}"
+      end
+    end
+  end
+
+  def trains_by_type(type)
+    self.trains.select { |train| train.type if train.type == type }.count
+  end
+
+  def take_the_train(train)
+    self.trains << train
+    puts "Поезд следующий по маршруту #{train.route.stations.first.title} - #{train.route.stations.last.title} прибыл на станцию #{self.title}"
+  end
+
+  def send_train(train)
+    if self.trains.include?(train) 
+      self.trains.delete(train) 
+
+      puts "Поезд отправился со станции #{self.title}"
+    else
+      puts "Такого поезда на станции #{self.title} нет."
+    end
+  end
+end
