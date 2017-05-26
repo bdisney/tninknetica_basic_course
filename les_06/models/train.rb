479e8bb804f8af7bfd8cf8bda1743c6e686fe856
disne_y <bold.disney@gmail.com>
2017-05-26 15:47:01 +0300

    Add train number validation methods

--- les_06/models/train.rb
+++ les_06/models/train.rb
@@ -12,10 +12,17 @@ class Train
 
   def initialize(number)
     @number = number
+    validate!
     @carriages = []
     @speed = INITIAL_SPEED
     @@trains[number] = self
     register_instance
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
   end
 
   def self.find_by(number)
@@ -30,7 +37,6 @@ class Train
     speed.zero? ? unhook_carriage! : (puts 'Прежде чем отцепить вагон остановите поезд.') 
   end
 
-  #Присвоение маршрута
   def set_route(route)
     self.current_station.send_train(self) if self.current_station
     @route = route
@@ -41,7 +47,6 @@ class Train
     gets
   end
 
-  #Перемещение по маршруту
   def move(destination)
     if destination 
       increase_speed(5)
@@ -66,41 +71,32 @@ class Train
     current_station == self.route.stations.first ? ( puts 'Конечная' ) : self.route.stations[position - 1]
   end
 
-  #Необязательный метод
   def about
     puts "Тип: #{TYPE[self.type]}, вагонов: #{self.carriages.count}, скорость: #{self.speed}"
   end
 
   protected
 
-  # #Вызов метода должен осуществляться только из метода move и невозможен из вне
-  # def move_to(sequent_station)
-  #   increase_speed(5)
-  #   self.current_station.send_train(self)
-  #   increase_speed(55)
-  #   self.current_station = sequent_station
-  #   stop
-  # end
+  def validate!
+    raise 'Недопустимый формат номера.' if self.number !~ NUMBER_FORMAT
+    raise "Поезд с номером #{number} уже существует." if @@trains.has_key?(number)
+    true
+  end
 
-  #Управление скоростью возможно при перемещении между станциями в рамках маршрута и...
-  #...в текущей реализации, только из метода move
   def increase_speed(value)
     value.positive? ? @speed = value : (puts 'Некорректное значение для увеличения скорости.')
   end
 
-  #см. комментарий выше
   def stop
     @speed = 0
   end
 
-  #Текущая станция назначается только при присвоении маршрута и изменяется при перемещении между станциями маршрута.
   def current_station=(station)
     stop
     station.take_the_train(self)
     @current_station = station
   end
 
-  #Скрытие деталей реализации. Вызов осуществляется только из метода unhook_carriage после прохождения необходимых проверок
   def unhook_carriage!
     if self.carriages.count.positive? 
       self.carriages.pop
@@ -110,7 +106,6 @@ class Train
     end
   end
 
-  #Вызов осуществляется только внутри класса Train и его подклассов
   def current_speed
     self.speed
   end
