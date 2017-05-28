f747e081d393adf1fde65c9d3e17d32d046fda26
disne_y <bold.disney@gmail.com>
2017-05-28 19:20:59 +0300

    Add volume methods for CargoCarriage

--- les_07/models/cargo_carriage.rb
+++ les_07/models/cargo_carriage.rb
@@ -1,7 +1,21 @@
 require_relative 'carriage.rb'
 
 class CargoCarriage < Carriage 
-  def initialize
+  def initialize(volume)
     @type = :cargo
+    @volume = volume
+    @used_volume = 0
+  end
+
+  def available
+    @volume - self.used
+  end
+
+  def fill_in(volume)
+    self.available > volume ? @used_volume += volume : ( raise 'Недостаточно свободного места' )
+  end
+
+  def used
+    @used_volume
   end
 end
