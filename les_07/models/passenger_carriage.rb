d2dcd04622a1a4a0aa4a613a03879869b9abd4af
disne_y <bold.disney@gmail.com>
2017-05-28 19:22:10 +0300

    Add seats methods for PassengerCarriage

--- les_07/models/passenger_carriage.rb
+++ les_07/models/passenger_carriage.rb
@@ -1,7 +1,21 @@
 require_relative 'carriage.rb'
 
 class PassengerCarriage < Carriage 
-  def initialize
+  def initialize(seats)
     @type = :passenger
+    @seats = seats.to_i
+    @taken = 0
+  end
+
+  def available
+    @seats - self.taken
+  end
+
+  def take_the_seat
+    self.available.positive? ? @taken += 1 : ( raise 'Свободных мест нет.' )
+  end
+
+  def taken
+    @taken
   end
 end
