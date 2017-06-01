1a6b8874d56604a2bf8daa1c884310f2237e6ddd
disne_y <bold.disney@gmail.com>
2017-06-01 14:58:31 +0300

    Add Accessors module with attr_accessor_with_history method

---
+++ les_09/modules/accessors.rb
@@ -0,0 +1,25 @@
+module Accessors
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def attr_accessor_with_history(*args)
+      args.each do |arg|
+        var_name = "@#{arg}".to_sym
+
+        define_method(arg) { instance_variable_get(var_name) }
+
+        define_method("#{arg}=".to_sym) do |value|
+          instance_variable_set(var_name, value)
+
+          @var_history ||= {}
+          @var_history[var_name] ||= []
+          @var_history[var_name] << value
+        end
+
+        define_method("#{arg}_history") { @var_history[var_name] }
+      end
+    end
+  end
+end
